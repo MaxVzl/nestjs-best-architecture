38e42bbc83c2eb7484b4275cf1828ef686dd3d77
MV <mv@scrinfo.net>
2025-11-19 12:34:26 +0100

    Refactor TenantsRepository constructor to use DataSource for entity management

--- src/modules/tenants/repositories/tenants.repository.ts
+++ src/modules/tenants/repositories/tenants.repository.ts
@@ -1,15 +1,11 @@
 import { Injectable } from "@nestjs/common";
-import { Repository } from "typeorm";
+import { DataSource, Repository } from "typeorm";
 import { Tenant } from "../entities/tenant.entity";
-import { InjectRepository } from "@nestjs/typeorm";
 
 @Injectable()
 export class TenantsRepository extends Repository<Tenant> {
-  constructor(
-    @InjectRepository(Tenant)
-    private readonly tenantRepository: Repository<Tenant>,
-  ) {
-    super(tenantRepository.target, tenantRepository.manager, tenantRepository.queryRunner);
+  constructor(private readonly dataSource: DataSource) {
+    super(Tenant, dataSource.createEntityManager());
   }
   
   async findOneByName(name: string): Promise<Tenant | null> {
