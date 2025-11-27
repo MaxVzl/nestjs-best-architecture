b3e958ae5260d8a5f71cb0c7cfeba0feea2a5285
MV <mv@scrinfo.net>
2025-11-27 12:13:33 +0100

    Remove console log for Queue module initialization in queue.ts

--- src/modules/queue/queue.ts
+++ src/modules/queue/queue.ts
@@ -3,7 +3,6 @@ import { QueueModule } from './queue.module';
 
 async function bootstrap() {
   const app = await NestFactory.createApplicationContext(QueueModule);
-  console.log('Queue module initialized');
   await app.init();
 }
 bootstrap();
